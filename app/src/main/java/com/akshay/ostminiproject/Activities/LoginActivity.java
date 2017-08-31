7979e7f82617b7111f2c1f6bc42dc0b5659bf292
Akshay Shah <akshah1997@gmail.com>
2017-08-31 16:43:11 +0530

    Removed Toolbar from Login and Password Reset activities

--- app/src/main/java/com/akshay/ostminiproject/Activities/LoginActivity.java
+++ app/src/main/java/com/akshay/ostminiproject/Activities/LoginActivity.java
@@ -37,9 +37,6 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
-
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
