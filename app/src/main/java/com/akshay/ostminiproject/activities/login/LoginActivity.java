e5203dd57017301c42fac85b423898b15c10d3b4
saintlyzero <shubham.dalvi97@gmail.com>
2017-09-17 21:18:49 +0530

    Logging according to user typ

--- app/src/main/java/com/akshay/ostminiproject/activities/login/LoginActivity.java
+++ app/src/main/java/com/akshay/ostminiproject/activities/login/LoginActivity.java
@@ -14,11 +14,26 @@ import android.widget.Toast;
 import com.akshay.ostminiproject.R;
 import com.akshay.ostminiproject.activities.notificationmsg.app.Config;
 import com.akshay.ostminiproject.activities.student.MainNavigation;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.android.volley.Request.Method.POST;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,12 +45,64 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String SEND_URL="http://ostminiproject.000webhostapp.com/getUserType.php";
 
         auth = FirebaseAuth.getInstance();
+        final String[] fname = new String[1];
+        final String[] lname = new String[1];
+        final String[] enrollmentNo = new String[1];
+        final String[] userType = new String[1];
+        final String[] fireBaseUid = new String[1];
 
         if(auth.getCurrentUser()!= null) {
             // TODO: get the user type based on uid and open the activity accordingly. Remember this is to be done at 2 places
-            String uid = auth.getCurrentUser().getUid();
+            final String uid = auth.getCurrentUser().getUid();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, SEND_URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    // Toast.makeText(MobileDetails.this, response, Toast.LENGTH_LONG).show();
+
+                    try
+                    {
+                        JSONObject jsonRootObject = new JSONObject(response);
+                        JSONArray jsonArray = jsonRootObject.optJSONArray("data");
+                        JSONObject jsonObject= jsonArray.getJSONObject(0);
+                        fireBaseUid[0] =jsonObject.optString("firebaseuid").toString();
+                        enrollmentNo[0] =jsonObject.optString("enrollment_number").toString();
+                        fname[0] =jsonObject.optString("fname").toString();
+                        lname[0] =jsonObject.optString("lname").toString();
+                        userType[0] =jsonObject.optString("user_type").toString();
+
+                        Toast.makeText(LoginActivity.this,fname[0]+"\nUser Type: "+userType[0], Toast.LENGTH_LONG).show();
+
+
+
+
+                    }
+                    catch (Exception e)
+                    {
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String,String>();
+                    params.put("username",uid);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+
+
             // if student TODO: make necessary changes based on type of user
             startActivity(new Intent(LoginActivity.this, MainNavigation.class));
             FirebaseMessaging.getInstance().subscribeToTopic(Config.TOPIC_GLOBAL);
